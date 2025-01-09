3061dcaedd16ab9f59148f7065c6eed111db9fb4
zebu12 <jeanpierre.salum@outlook.com>
2025-01-09 09:31:27 +0100

    Initial Commit with update1

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,8 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Inversion of Control and Dependency injection in Spring");
         System.out.println("Hello Jean-Pierre");
+
     }
 }
