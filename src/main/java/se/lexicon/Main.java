30fde5a4f25490d05b9adedaa34fda84fb189dd3
zebu12 <jeanpierre.salum@outlook.com>
2025-01-09 09:28:22 +0100

    Initial Commit with update

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,6 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Jean-Pierre");
     }
 }
